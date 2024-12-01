e93bda1881f0a924ebcc4d029464e30f176545dc
kakani17481a0566 <84347625+kakani17481a0566@users.noreply.github.com>
2024-12-01 08:30:02 +0530

    Update MEDICINES.sql

---
+++ MEDICINES.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[MEDICINES] (
+    S_NO FLOAT NULL,          -- Sequential number
+    DATE DATETIME NULL,       -- Timestamp
+    MEDICINES_FEE FLOAT NULL, -- Fee for medicines
+    MEDICINES_TYPE NVARCHAR(255) NULL -- Type of medicine
+);
